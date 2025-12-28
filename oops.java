ed4f4ad673c571d79b5afadac78f82016aeee477
Pragyaa-pg <aaryapg17@gmail.com>
2025-12-28 23:48:19 +0530

    add oops

---
+++ oops.java
@@ -0,0 +1,21 @@
+// package Java;
+
+class Student{
+    String name;
+    int age;
+
+    public void printInfo(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+    }
+}
+public class oops {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.name = "aarya";
+        s1.age =24;
+
+        s1.printInfo();
+    }
+    
+}
