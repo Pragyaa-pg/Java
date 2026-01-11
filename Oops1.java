7fbce3e30096b7904b9e832b3e67c4412dbd8feb
Pragyaa-pg <aaryapg17@gmail.com>
2026-01-11 19:03:52 +0530

    add oops1

--- Oops1.java
+++ Oops1.java
@@ -9,16 +9,21 @@ class Student{
         System.out.println(course);
         System.out.println(id);
     }
+    Student(String name,int age,int id,String course){
+        System.out.println("constructor called");
+        this.name =name;
+        this.age =age;
+        this.course =course;
+        this.id =id;
+    }
 }
 public class Oops1 {
     public static void main(String[] args) {
-        
-    
-    Student s1 = new Student();
-    s1.name ="rupesh";
-    s1.age =20;
-    s1.course="btech";
-    s1.id=2023;
+    Student s1 = new Student("pragya",20,2023,"btech");
+    // s1.name ="rupesh";
+    // s1.age =20;
+    // s1.course="btech";
+    // s1.id=2023;
     s1.printInfo();
     }
     
